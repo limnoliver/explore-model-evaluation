028e72c52a8ad914eba0415b673cde522928d846
Atshan <ratshan>
2020-07-08 10:59:22 -0500

    Setting the number of rounded digits to 2. and Giving the user the option to select the number of digits when rounding the error.

--- code/starter_script - Copy.R
+++ code/starter_script - Copy.R
@@ -14,8 +14,8 @@ rgnc_by_seg <- rgnc_dat_filter %>% group_by(seg_id_nat)
 
 ## Absolute Residual Error: calculating Mean Absolute Error (MAE) metric to compare both models prediction data vs observed data. We found the mae by finding the sum of the absolute value difference in predicted and observed temperature. Then divided the sum by the n row in grouped data.
 
-cal_mae <- function(observe_data, predict_data) {
-    mae = round((mean(abs(observe_data - predict_data), na.rm = TRUE )), digits = 2)
+cal_mae <- function(observe_data, predict_data, n_digits = 2) {
+    mae = round((mean(abs(observe_data - predict_data), na.rm = TRUE )), digits = n_digits)
     return(mae)
 }
 mae <- rgnc_by_seg %>%  
@@ -23,8 +23,8 @@ mae <- rgnc_by_seg %>%
             MAE_Hybrid_Model = cal_mae(temp_c, rgcn2_full_temp_c))
 
 # Absolute Residual Error: Root Mean Square Error (RMSE) square the difference in predicted and observed temperature.  sum the squared values, then divided by n of row. Finally,took the square root
-cal_rmse <- function(observe_data, predict_data){
-  rmse <- round(sqrt(mean((observe_data - predict_data) ^2, na.rm = TRUE )), digits = 2)
+cal_rmse <- function(observe_data, predict_data, n_digits = 2 ){
+  rmse <- round(sqrt(mean((observe_data - predict_data) ^2, na.rm = TRUE )), digits = n_digits)
   return(rmse)
 }
 rmse <- rgnc_by_seg %>%
@@ -33,8 +33,8 @@ rmse <- rgnc_by_seg %>%
 
 # Relative Parameter: Mean Absolute Relative Error (MARE) metric to compare both models prediction data vs observed data. We found MARE by: 1) dividing the difference between the predicted and observed data by the observed measurements. 2) sum the division answer. 3) divide by number of rows in grouped data.  
 ## mare metric when removing inf terms from ratio of temp diff and observed temp. 
-cal_mare <- function(observe_data, predict_data){
-  mare <- round(mean((abs(observe_data - predict_data) / observe_data), na.rm = TRUE), digits = 2)
+cal_mare <- function(observe_data, predict_data, n_digits = 2){
+  mare <- round(mean((abs(observe_data - predict_data) / observe_data), na.rm = TRUE), digits = n_digits)
   return(mare)
 }  
 mare <- rgnc_by_seg %>%
